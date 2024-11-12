aa1d8d31ae53351117e7ab557d4d3c7ff9dd63da
Shreyas Kothari <spkothari0@gmail.com>
2024-11-12 18:39:44 -0500

    making code compatible with SNS trigger

--- ServerlessEmailLambda/src/main/java/org/shreyas/EmailHandler.java
+++ ServerlessEmailLambda/src/main/java/org/shreyas/EmailHandler.java
@@ -3,36 +3,50 @@ package org.shreyas;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.SNSEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mailgun.api.v3.MailgunMessagesApi;
 import com.mailgun.client.MailgunClient;
 import com.mailgun.model.message.Message;
+import com.mailgun.model.message.MessageResponse;
 import org.shreyas.model.EmailRequest;
 
 
-public class EmailHandler implements RequestHandler<EmailRequest, String> {
+public class EmailHandler implements RequestHandler<SNSEvent, String> {
     private static final String mailGunDomainName = System.getenv("MAIL_GUN_DOMAIN_NAME");
     private static final String mailGunApiKey = System.getenv("MAIL_GUN_API_KEY");
 
-    public static void sendEmail(String sender, String subject, String body) {
+    public static MessageResponse sendEmail(String sender, String subject, String body) {
         MailgunMessagesApi mailgunMessagesApi = MailgunClient.config(mailGunApiKey)
                 .createApi(MailgunMessagesApi.class);
         Message message = Message.builder()
-                .from("noreply@shreyaskothari.me")
+                .from("noreply@" + mailGunDomainName)
                 .to(sender)
                 .subject(subject)
                 .text(body)
                 .build();
 
-        mailgunMessagesApi.sendMessage(mailGunDomainName, message);
+        return mailgunMessagesApi.sendMessage(mailGunDomainName, message);
     }
 
     @Override
-    public String handleRequest(EmailRequest o, Context context) {
-
+    public String handleRequest(SNSEvent event, Context context) {
+        ObjectMapper objectMapper = new ObjectMapper();
         LambdaLogger log = context.getLogger();
-        log.log("Payload is: " + o);
-        log.log("Environment variables are: MAIL_GUN_DOMAIN_NAME"+mailGunDomainName);
-        log.log("Environment variables are: MAIL_GUN_API_KEY"+mailGunApiKey);
-        return o.toString();
+
+        for(SNSEvent.SNSRecord record: event.getRecords()){
+            String message = record.getSNS().getMessage();
+            log.log("Message from SNS: " + message);
+            try {
+                EmailRequest o = objectMapper.readValue(message, EmailRequest.class);
+                MessageResponse response = sendEmail(o.getRecipient(), o.getSubject(), o.getMessage());
+                log.log("Email Sending service response is: " +response.getMessage());
+                return response.getMessage();
+            } catch (JsonProcessingException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return "No valid email request found in the input payload";
     }
 }
